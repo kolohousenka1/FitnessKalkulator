5932777ded8e10d86b0e40f4a7beec124e4134eb
Sminoo <simisrob@gmail.com>
2025-01-15 16:44:42 +0100

    Začíname naše dobrodružstvo

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from signal import valid_signals
 from tkinter import ttk, messagebox
 
 
@@ -8,13 +9,30 @@ class App:
         self.root.title("Moja Tkinter Aplikácia")
         self.root.geometry("800x600")  # Nastavenie veľkosti okna
 
-        # Hlavný obsah
+        self.vstup_var = tk.StringVar()
+        self.vystup_var = tk.StringVar()
+
+
         self.create_widgets()
 
     def create_widgets(self):
 
         label = ttk.Label(self.root, text="tkinter zaklad", font=("Arial", 16))
         label.pack(pady=20)
+        vstup = ttk.Entry(self.root, textvariable=self.vstup_var, font=("Arial", 16))
+        vstup.pack(pady=20)
+        tlacidlo = ttk.Button(self.root, command=self.urob, text="Stlač")
+        tlacidlo.pack(pady=20)
+        vystup = ttk.Label(self.root, textvariable=self.vystup_var, font=("Arial", 16))
+        vystup.pack(pady=20)
+
+    def urob(self):
+        vstup_hodnota = self.vstup_var.get()
+        if vstup_hodnota.strip():
+            self.vystup_var.set(vstup_hodnota)
+        else:
+            self.vystup_var.set("Zadajte text.")
+
 
 
 
